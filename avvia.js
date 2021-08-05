4aac61131352737c9b342da33b1a493975bf4f74
mbarbieri98 <barba.0098@gmail.com>
2021-08-05 13:15:50 +0200

    Ip universale(?)

--- avvia.js
+++ avvia.js
@@ -37,6 +37,25 @@ getGeofences();
 
 getGeofencesVietate();
 
+var set_ip_address = require('set-ip-address')
+
+var eth0 = {
+    interface: 'eth0',
+    ip_address: '10.0.0.1',
+    prefix: 20,
+    gateway: '10.0.0.1',
+    nameservers: ['8.8.8.8'],
+    optional: true // (netplan) - dont wait for interfaces to avoid boot delay
+}
+
+var eth1 = {
+    interface: 'eth1',
+        dhcp: true
+}
+
+set_ip_address.configure([eth0, eth1]).then(() => console.log('done writing config files'));
+
+
 app.listen(3000, () => {
     console.log("Application started and Listening on port 3000");
 });
