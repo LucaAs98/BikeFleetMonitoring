94e6edf565dac2880f246e9572ad6ced98b58531
andre <andreacirina1@hotmail.it>
2021-08-19 11:16:25 +0200

    Gestione delle bici che risiedono al di fuori del range limite (5km)

--- get_bici_real_time.js
+++ get_bici_real_time.js
@@ -3,24 +3,28 @@ getBiciRealTime();
 async function getBiciRealTime() {
     const response = await fetch('/bici_real_time');
     const data = await response.json();
-    if (!window.abortLoopBikesRealTime){
+    if (!window.abortLoopBikesRealTime) {
         addRealTime(data);
     }
-
 }
 
-function addRealTime(data) {
+async function addRealTime(data) {
     var jsonFeatures = [];
+
+    const responseBiciFuoriRange = await fetch('/bici_fuori_range');
+    const biciFuoriRange = await responseBiciFuoriRange.json();
 
     data.forEach(function (point) {
         var lat = point.lat;
         var lon = point.long;
+        var id = point.id;
 
         var feature = {
             type: 'Feature',
             properties: {
                 lat: lat,
                 lon: lon,
+                id: id
             },
             geometry: {
                 type: 'Point',
@@ -32,21 +36,35 @@ function addRealTime(data) {
 
     var geoJson = {type: 'FeatureCollection', features: jsonFeatures};
 
-    var greenIcon = new L.Icon({
-        iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
-        shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
-        iconSize: [25, 41],
-        iconAnchor: [12, 41],
-        popupAnchor: [1, -34],
-        shadowSize: [41, 41]
-    });
-
     window.layerBiciRealTime = L.geoJson(geoJson, {
         pointToLayer: function (feature, latlng) {
-            return L.marker(latlng,{
-                icon: greenIcon,
+            return L.marker(latlng, {
+                icon: getIcon(feature),
             });
         },
     });
+
+    function getIcon(feature) {
+        var color = 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png';
+        console.log(biciFuoriRange)
+
+        for (var bici of biciFuoriRange) {
+            if (bici.id === feature.properties.id ) {
+                color = 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png'
+                break;
+            }
+        }
+        return (new L.Icon({
+            iconUrl: color,
+            shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+            iconSize: [25, 41],
+            iconAnchor: [12, 41],
+            popupAnchor: [1, -34],
+            shadowSize: [41, 41]
+        }));
+
+    }
+
     window.layerBiciRealTime.addTo(mymap);
 }
+
