83a2f61ceb3f1e93234ae5b39132d9dcb915fecf
mbarbieri98 <barba.0098@gmail.com>
2021-08-20 17:12:31 +0200

    tragitto termina in una rastrelliera

--- start_simulation.js
+++ start_simulation.js
@@ -128,98 +128,136 @@ async function avvia() {
     let x0 = 11.343083149519329; //longitudine centro
     let y0 = 44.501726198465064; //latitudine centro
     let rd = 1300 / 111300;
+
+
+    let arrTerminazione = [];
+
+    for (g = 0; g < maxUtenti; g++) {
+        let terminazione = {
+            coordinate: {},
+            terminato: false
+        }
+        arrTerminazione.push(terminazione);
+    }
     sendPositions();
 
     async function sendPositions() {
 
+
         for (const prenotazione of arrPrenotazioni) {
+
             const n = arrPrenotazioni.indexOf(prenotazione);
 
-            formBody = [];
+            if (!(arrTerminazione[n].terminato)) {
 
-            let lat;
-            let long;
+                formBody = [];
 
-            if (geom[n].length === 0) {
-                lat = latRastrellieraPartenza;  //posizione rastrelliera di partenza
-                long = longRastrellieraPArtenza;
-            } else {
-                //calcolo coordinate random
-                let u = Math.random();
-                let v = Math.random();
+                let lat;
+                let long;
 
-                let w = rd * Math.sqrt(u)
-                let t = 2 * Math.PI * v
-                let x = w * Math.cos(t)
-                let y = w * Math.sin(t)
+                if (geom[n].length === 0) {
+                    lat = latRastrellieraPartenza;  //posizione rastrelliera di partenza
+                    long = longRastrellieraPArtenza;
 
-                long = (x / Math.cos(y0)) + x0;
-                lat = y + y0;
+                } else {
+                    //calcolo coordinate random
+                    let u = Math.random();
+                    let v = Math.random();
+
+                    let w = rd * Math.sqrt(u)
+                    let t = 2 * Math.PI * v
+                    let x = w * Math.cos(t)
+                    let y = w * Math.sin(t)
+
+                    long = (x / Math.cos(y0)) + x0;
+                    lat = y + y0;
+
+                }
+
+                //Creazione della stringa con tutte le geometrie
+                if (geom[n].length > 0) {
+                    geom[n] += ",";
+
+                }
+                geom[n] += "[" + long + "," + lat + "]";
+
+                //add della posizione
+                let coordinates = {
+                    long: long,
+                    lat: lat,
+                    id: prenotazione.bici,
+
+                }
+
+                arrTerminazione[n].coordinate = coordinates;
+
+                for (let property in coordinates) {
+                    let encodedKey = encodeURIComponent(property);
+                    let encodedValue = encodeURIComponent(coordinates[property]);
+                    formBody.push(encodedKey + "=" + encodedValue);
+
+                }
+                formBody = formBody.join("&");
+
+
+                fetch('/addPosizione', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/x-www-form-urlencoded'
+                    },
+                    body: formBody,
+                });
+
             }
-
-            //Creazione della stringa con tutte le geometrie
-            if (geom[n].length > 0) {
-                geom[n] += ",";
-            }
-            geom[n] += "[" + long + "," + lat + "]";
-
-            //add della posizione
-            let coordinates = {
-                long: long,
-                lat: lat,
-                id: prenotazione.bici,
-            }
-
-            for (let property in coordinates) {
-                let encodedKey = encodeURIComponent(property);
-                let encodedValue = encodeURIComponent(coordinates[property]);
-                formBody.push(encodedKey + "=" + encodedValue);
-            }
-            formBody = formBody.join("&");
-
-
-            fetch('/addPosizione', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/x-www-form-urlencoded'
-                },
-                body: formBody,
-            });
-
-
         }
-        i++;
+
         if (i < maxSalti) {//arrayGeom.length){
+            i++;
             setTimeout(sendPositions, 1000);
         } else {
-            terminaNoleggio();
-            //se la posizione finale non è vicino a una rastrelliera, lo spostamento continua
-            /* const response = await fetch('/checkDistance?lat=' + lat + '&lng=' + long, {
-                 method: 'GET',
-             });
-             const data = await response.json();
-             let id;
-             console.log(data)
-             if (data[0] !== undefined) {
-                 id = data[0].id;
-                 terminaNoleggio(id);
-             } else {
-                 setTimeout(sendPositions, 1000);
-             }*/
+            let cont = 0;
+            for (const item of arrTerminazione) {
+                const n = arrTerminazione.indexOf(item);
+                if (!item.terminato) {
+
+
+                    //se la posizione finale non è vicino a una rastrelliera, lo spostamento continua
+                    const response = await fetch('/checkDistance?lat=' + item.coordinate.lat + '&lng=' + item.coordinate.long, {
+                        method: 'GET',
+                    });
+                    const data = await response.json();
+                    let id;
+
+                    if (data[0] !== undefined) {
+                        id = data[0].id;
+                        cont++;
+                        item.terminato = true;
+                        terminaNoleggio(id, n);
+                    }
+
+                } else {
+                    cont++;
+                }
+
+            }
+            if (cont < maxUtenti) {
+                setTimeout(sendPositions, 1000);
+            }
+
         }
     }
 
     /* Faccio terminare il noleggio. */
-    async function terminaNoleggio() {
+    async function terminaNoleggio(id, n) {
 
-        for (const prenotazione of arrPrenotazioni) {
-            const n = arrPrenotazioni.indexOf(prenotazione);
+
+        if ((arrTerminazione[n].terminato)) {
 
             let noleggioTerminato = {
-                codNoleggio: prenotazione.cod,
-                bici: prenotazione.bici,
+                codNoleggio: arrPrenotazioni[n].cod,
+                bici: arrPrenotazioni[n].bici,
                 geom: "[" + geom[n] + "]",
-                rastrelliera: prenotazione.ras
+                rastrelliera: id
             }
 
             formBody = [];
